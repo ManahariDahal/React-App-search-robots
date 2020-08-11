4ca0bcd1235e58f704468f4d9272937ca56fc273
ManahariDahal <manahari.dahal1@gmail.com>
2020-08-10 22:19:32 -0600

    Searched bar improved

---
+++ src/robots.js
@@ -0,0 +1,61 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Alcina Schade',
+		email: 'alcinaschade@gmail.com',
+	},
+
+	{
+		id: 2,
+		name: 'Angelica Kale',
+		email: 'angelicakale@gmail.com',
+	},
+
+	{
+		id: 3,
+		name: 'Sayaka Blake',
+		email: 'sayakablake@gmail.com',
+	},
+
+	{
+		id: 4,
+		name: 'Prue Leon',
+		email: 'prueleon@gmail.com',
+	},
+
+	{
+		id: 5,
+		name: 'Mortianna Weaver',
+		email: 'mortiannaweaver@gmail.com',
+	},
+
+	{
+		id: 6,
+		name: 'Serafina Myrtle',
+		email: 'serafinamyrtle@gmail.com',
+	},
+
+	{
+		id: 7,
+		name: 'Desiderata Cleary',
+		email: 'desideratacleary@gmail.com',
+	},
+
+	{
+		id: 8,
+		name: 'Clarissa Kurusugawa',
+		email: 'clarissakurusugawa@gmail.com',
+	},
+
+	{
+		id: 9,
+		name: 'Drolta Overbrook',
+		email: 'droltaoverbrook@gmail.com',
+	},
+
+	{
+		id: 10,
+		name: 'Winifred Pomfrey',
+		email: 'winifredpomfrey@gmail.com'
+	},
+]
