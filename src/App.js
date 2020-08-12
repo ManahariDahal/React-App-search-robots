e391177d5a5e426a8bdac3d2a89e76e5127c25e9
ManahariDahal <manahari.dahal1@gmail.com>
2020-08-12 15:10:51 -0600

    Updated the search scrolling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import Card from './Card';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import { robots } from './robots';
 import './App.css';
 
@@ -10,9 +11,15 @@ class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => this.setState({robots: users }));
 	}
 
 	onSearchChange = (event) => {
@@ -23,13 +30,24 @@ class App extends Component {
 		const filteredRobots = this.state.robots.filter (robots => {
 			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		})
-		return (
-		<div className = 'tc'>
-			<h1 className='f1'> Robot Friends </h1>
-			<SearchBox searchChange = {this.onSearchChange} />
-			<CardList robots = {filteredRobots} />
-		</div>
-	);
+		if (this.state.robots.length === 0) 
+		{
+			return <h1> Loading </h1>
+		}
+		else {
+			return (
+				<div className = 'tc'>
+					<h1 className='f1'> Robot Friends </h1>
+					<SearchBox searchChange = {this.onSearchChange} />
+					<Scroll>
+						<CardList robots = {filteredRobots} />
+					</Scroll>
+				</div>
+		);
+	}
+
+
+		
 	}
 }
 
