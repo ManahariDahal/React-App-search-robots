aa5e6be781d7b0d546afea7d1c60d80bd206a53c
ManahariDahal <manahari.dahal1@gmail.com>
2020-08-14 16:09:04 -0600

    Updates

---
+++ precache-manifest.b20027c67f11161508c4183e06226bf6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7807e9a27b9c08f4c9b9ebd4e37e9bb",
+    "url": "/React-App-search-robots/index.html"
+  },
+  {
+    "revision": "a649fd38ba8569e19384",
+    "url": "/React-App-search-robots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d94ec6dc00eee2d752b6",
+    "url": "/React-App-search-robots/static/css/main.ff853b43.chunk.css"
+  },
+  {
+    "revision": "a649fd38ba8569e19384",
+    "url": "/React-App-search-robots/static/js/2.907bdc06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App-search-robots/static/js/2.907bdc06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d94ec6dc00eee2d752b6",
+    "url": "/React-App-search-robots/static/js/main.08fc7db5.chunk.js"
+  },
+  {
+    "revision": "cb6d2870c3f91647e3c8",
+    "url": "/React-App-search-robots/static/js/runtime-main.4c1bd208.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/React-App-search-robots/static/media/SEGA.82e09f78.TTF"
+  }
+]);
